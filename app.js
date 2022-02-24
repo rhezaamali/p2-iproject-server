f436ede11d424dd8fd74fb7bebcf13a73cf7e2ac
rhezaamali <rhezaamali@gmail.com>
2022-02-24 12:07:42 +0700

    deploy

--- app.js
+++ app.js
@@ -21,4 +21,12 @@ app.use("/", router);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log("Running in port", PORT));
+// app.listen(PORT, () => console.log("Running in port", PORT));
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
